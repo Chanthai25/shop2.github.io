63dee5938a8f1510bca19f268acb76b5bef30d44
Thai <148747170+Chanthai25@users.noreply.github.com>
2023-12-15 22:47:42 +0700

    Add files via upload

---
+++ Shop_detail/type_boy.js
@@ -0,0 +1,164 @@
+const Item_list=[
+    {
+         "id":"1",
+        "Img":"https://zandokh.com/image/cache/catalog/products/2023-09/2122306173/Outdoor/Nylon-Vest%20(6)-cr-450x672.jpg",
+        "tille":"HamZa NOUASRIA",
+        "del":"70$",
+        "price":"60$",
+        "star":"<i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i><i class='fa-solid fa-star'> </i>",
+        "type":"boy"
+
+   },
+   {
+        "id":"2",
+        "Img":"https://zandokh.com/image/cache/catalog/products/2023-11/2222309280/Outdoor/Croped-T-Shirt%20(1)-cr-450x672.jpg",
+        "tille":"Anna Nekrashevich",
+        "del":"80$",
+        "price":"70$",
+        "star":"<i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i><i class='fa-solid fa-star'> </i>",
+        "type":"gril"
+   },
+   {
+        "id":"2",
+        "Img":"https://zandokh.com/image/cache/catalog/products/2023-11/2122309221/OUTDOOR/CARGO-TROUSER%20(1)-cr-450x672.jpg",
+        "tille":"dayong tien",
+        "del":"100$",
+        "price":"90$",
+        "star":"<i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i><i class='fa-solid fa-star'> </i>",
+        "type":"boy"
+   },
+   {
+        "id":"2",
+        "Img":"https://zandokh.com/image/cache/catalog/products/2023-10/2122308224/Outdoor/Vest-Tank-Top%20(1)-cr-450x672.jpg",
+        "tille":"Grailify",
+        "del":"120$",
+        "price":"100$",
+        "star":"<i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i><i class='fa-solid fa-star'> </i>",
+        "type":"boy"
+   },
+   {
+        "id":"2",
+        "Img":"https://zandokh.com/image/cache/catalog/products/2023-11/2222306197/Open-Back-Top%20(2)-cr-450x672.jpg",
+        "tille":"Grailify",
+        "del":"120$",
+        "price":"100$",
+        "star":"<i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i><i class='fa-solid fa-star'> </i>",
+        "type":"gril"
+   },
+   {
+        "id":"1",
+        "Img":"https://zandokh.com/image/cache/catalog/products/2023-09/2222309349/OUTDOOR/TANK-TOP%20(3)-cr-450x672.jpg",
+        "tille":"Grailify",
+        "del":"120$",
+        "price":"110$",
+        "star":"<i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i><i class='fa-solid fa-star'> </i>",
+        "type":"gril"
+
+   },
+   {
+        "id":"1",
+        "Img":"https://zandokh.com/image/cache/catalog/products/2023-11/2222311324/TOP-cr-450x672.jpg",        
+        "tille":"Grailify",
+        "del":"100$",
+        "price":"90$",
+        "star":"<i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i><i class='fa-solid fa-star'> </i>",
+        "type":"gril"
+   },
+   {
+        "id":"2",
+        "Img":"https://zandokh.com/image/cache/catalog/products/2023-11/2222309344/T-Shirt-With-Print%20(3)-cr-450x672.jpg",       
+        "tille":"Grailify",
+        "del":"80$",
+        "price":"60$",
+        "star":"<i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i><i class='fa-solid fa-star'> </i>",
+        "type":"gril"
+   },
+   {
+        "id":"2",
+        "Img":"https://zandokh.com/image/cache/catalog/products/2023-10/2222309342/Cropped-T-Shirt-With-Print%20(4)-cr-450x672.jpg",
+                "tille":"MeakUp",
+        "del":"80$",
+        "price":"60$",
+        "star":"<i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i><i class='fa-solid fa-star'> </i>",
+        "type":"gril"
+   },
+   {
+        "id":"1",
+        "Img":"https://zandokh.com/image/cache/catalog/products/2023-10/22222307201/High-Waist-Shorts%20(1)-cr-450x672.jpg",
+                "tille":"Watch gril",
+        "del":"80$",
+        "price":"60$",
+        "star":"<i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i><i class='fa-solid fa-star'> </i>",
+        "type":"gril"
+   },
+   {
+        "id":"2",
+        "Img":"https://zandokh.com/image/cache/catalog/products/2023-11/2122309245/Outdoor/Shirt%20(2)-cr-450x672.jpg",
+                "tille":"wacth",
+        "del":"80$",
+        "price":"60$",
+        "star":"<i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i><i class='fa-solid fa-star'> </i>",
+        "type":"boy"
+   },
+   {
+        "id":"2",
+        "Img":"https://zandokh.com/image/cache/catalog/products/2023-10/2222308292/Cropped-Overshirt%20(2)-cr-450x672.jpg",
+        "tille":"wacth",
+        "del":"80$",
+        "price":"60$",
+        "star":"<i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i> <i class='fa-solid fa-star'> </i><i class='fa-solid fa-star'> </i>",
+        "type":"gril"
+   }
+];
+type();
+function type()
+{
+    var data="";
+    for (const i of Item_list) {
+     if(i.type=="boy"){
+          if(i.id==1){
+          data+=
+          `<div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 col-12 item-box">
+          <div class="box">
+            <div class="box-img"><img src="${i.Img}" alt=""></div>
+            <div class="box-text">
+              <h1> ${i.tille}</h1>
+              <p><del class="del">${i.del}</del> ${i.price}</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <div class="star">
+                <p>${i.star}</p>
+               
+              </div>
+            </div>
+          </div>
+      </div>
+     `;
+     }
+     else{
+          data+=
+          `    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 col-12 item-box">
+          <div class="box">
+            <div class="box-img"><img src="${i.Img}" alt=""></div>
+            <div class="box-text">
+              <h1>${i.tille}</h1>
+              <p><del class="del">${i.del}</del>${i.price}</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <div class="star">
+                <p>${i.star}</p>
+                <div class="discount"></div>
+              </div>
+            </div>
+            <div class="discount"> Discount</div>
+          </div>
+          
+      </div>
+     
+          `
+     }
+      }
+ 
+
+        
+    }
+    document.getElementById("item").innerHTML=data;
+}
